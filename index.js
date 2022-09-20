f66a2c40fd936aa0557448ca442eed46bee15bf6
TheObstyProjects <113110796+TheObstyProjects@users.noreply.github.com>
2022-09-21 01:30:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const txtarea = document.getElementById('txtarea');
+const output = document.getElementById('output');
+
+function input() {
+    output.innerHTML = txtarea.value;
+}
+
+function upper() {
+    output.innerHTML = txtarea.value.toUpperCase();
+}
+
+function lower() {
+    output.innerHTML = txtarea.value.toLowerCase();
+}
+
+function capitalize() {
+    const words = txtarea.value.split(" ");
+    
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    output.innerHTML = words[0];
+    
+}
+
+function cpytxt() {
+    let cpytxt = txtarea;
+    // Select the text field
+    txtarea.select();
+    txtarea.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(txtarea.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + txtarea.value);
+}
